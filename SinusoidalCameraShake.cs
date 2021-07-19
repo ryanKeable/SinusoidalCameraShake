dc25b2c58eb077ae82f3686664558c4a3535e515
ryanKeableMightyGames <77820647+ryanKeableMightyGames@users.noreply.github.com>
2021-07-19 18:39:36 +1000

    Update SinusoidalCameraShake.cs
    refactored to make it cleaner

--- SinusoidalCameraShake.cs
+++ SinusoidalCameraShake.cs
@@ -9,6 +9,7 @@ public class SinusoidalCameraShake : MonoBehaviour
     public float translationStrength = 1;
     public float rotationStrength = .5f;
 
+    const float rotStrengthModifier = 1.5f;
     float duration = 0;
     float timeElapsed = 0;
 
@@ -33,11 +34,19 @@ public class SinusoidalCameraShake : MonoBehaviour
         duration = shakeDuration;
         timeElapsed = 0;
 
-        SetShakePositions();
-        SetShakeRotations();
+        SetShakeDeltas();
     }
 
-    public void Shake(float transStrength, float rotStrength, float duration, int shakes)
+    public void Shake(float transStrength, float duration, int shakes)
+    {
+        shakeDuration = duration;
+        numberOfShakes = shakes;
+        translationStrength = transStrength;
+        rotationStrength = transStrength * rotStrengthModifier;
+
+        Shake();
+    }
+    public void Shake(float transStrength, float duration, int shakes, float rotStrength)
     {
         shakeDuration = duration;
         numberOfShakes = shakes;
@@ -55,10 +64,8 @@ public class SinusoidalCameraShake : MonoBehaviour
 
             if (timeElapsed < shakeDuration)
             {
-                float frequency = numberOfShakesCos(timeElapsed);
-                int roundedFrequency = Mathf.RoundToInt(numberOfShakesSin(timeElapsed));
 
-                ShakeCamera(timeElapsed, frequency, roundedFrequency);
+                ShakeCamera(timeElapsed);
             }
             else
             {
@@ -70,18 +77,29 @@ public class SinusoidalCameraShake : MonoBehaviour
         }
     }
 
-    void ShakeCamera(float time, float frequency, int roundedFrequency)
+    void ShakeCamera(float time)
     {
-        int increment = Mathf.FloorToInt(Frequency(time));
+        // this is the Sinusoidal Shake engine
+        // uses models in thie graph: https://www.desmos.com/calculator/t3eg3bc3xa
 
-        int index01 = roundedFrequency + increment;
-        int index02 = (1 - roundedFrequency) + increment;
+        // which shake we are upto
+        int shakeIncrement = Mathf.FloorToInt(Frequency(time));
 
+        // at each increment we need to determine the deltas to lerp between
+        // because we are using an Sinusoidal wave to drive our lerping we swap delta targets per shake increment using a sin wave as a LUT
+        int lerpDir = Mathf.RoundToInt(numberOfShakesSin(timeElapsed)); // returns 0 or 1 (forwards or backwards)
+        int deltaTarget01 = lerpDir + shakeIncrement;
+        int deltaTarget02 = (1 - lerpDir) + shakeIncrement;
+
+        // reduce strength of shake over time
         float tStrength = DampenStrength(time, translationStrength);
         float rStrength = DampenStrength(time, rotationStrength);
 
-        transform.localPosition = Vector3.Lerp(shakePositions[index01] * tStrength, shakePositions[index02] * tStrength, frequency);
-        transform.localEulerAngles = Vector3.Lerp(shakeRotations[index01] * rStrength, shakeRotations[index02] * rStrength, frequency);
+        // ocsilates beteen 0 and 1 over the durtation with decay
+        float lerpFrequency = numberOfShakesCos(timeElapsed);
+
+        transform.localPosition = Vector3.Lerp(shakePositions[deltaTarget01] * tStrength, shakePositions[deltaTarget02] * tStrength, lerpFrequency);
+        transform.localEulerAngles = Vector3.Lerp(shakeRotations[deltaTarget01] * rStrength, shakeRotations[deltaTarget02] * rStrength, lerpFrequency);
     }
 
     float numberOfShakesCos(float time)
@@ -100,11 +118,12 @@ public class SinusoidalCameraShake : MonoBehaviour
 
     float Frequency(float time, float pi = 1)
     {
-        float f = time * pi * numberOfShakes;
-        f *= 2 - (time / shakeDuration);
-        f /= shakeDuration;
+        float speed = time * pi * numberOfShakes;
+        float decay = 2 - (time / shakeDuration);
+        float frequency = (speed * decay) / shakeDuration;
+        frequency *= 0.75f; // 0.75f makes sure we ocsilate through 4 points 
 
-        return f * 0.75f; // 0.75f makes sure we ocsilate through 4 points 
+        return frequency;
     }
 
     float DampenStrength(float time, float strength)
@@ -120,49 +139,40 @@ public class SinusoidalCameraShake : MonoBehaviour
         return (value + 1) * .5f;
     }
 
-    void SetShakePositions()
-    {
-        shakePositions = new List<Vector3>();
-
-        int totalShakes = numberOfShakes - 1;
-
-        for (int i = 0; i < totalShakes; i++)
-        {
-            shakePositions.Add(RandomShakePosition());
-        }
-
-        shakePositions.Add(Vector3.zero); // finish at 0
-    }
-
-    Vector3 RandomShakePosition()
-    {
-        Vector3 unitVec3D = Random.insideUnitSphere;
-        unitVec3D *= translationStrength;
-
-        return unitVec3D;
-    }
-
-    void SetShakeRotations()
+    void SetShakeDeltas()
     {
         shakeRotations = new List<Vector3>();
+        shakePositions = new List<Vector3>();
 
-        int totalShakes = numberOfShakes - 1;
+        int totalShakes = numberOfShakes - 2; // do not count bookends
+
+        shakeRotations.Add(Vector3.zero); // start at 0
+        shakePositions.Add(Vector3.zero); // start at 0
 
         for (int i = 0; i < totalShakes; i++)
         {
-            shakeRotations.Add(RandomShakeRotation());
+            shakeRotations.Add(RandomRotationDelta());
+            shakePositions.Add(RandomPositionDelta());
         }
 
         shakeRotations.Add(Vector3.zero); // finish at 0
+        shakePositions.Add(Vector3.zero); // finish at 0
     }
 
-    Vector3 RandomShakeRotation()
+    Vector3 RandomRotationDelta()
     {
         Vector3 unitVec3D = Random.insideUnitSphere;
         float yForward = Mathf.Abs(unitVec3D.y); // make sure we are always facing forward
         unitVec3D *= rotationStrength;
 
         return unitVec3D;
+    }
 
+    Vector3 RandomPositionDelta()
+    {
+        Vector3 unitVec3D = Random.insideUnitSphere;
+        unitVec3D *= translationStrength;
+
+        return unitVec3D;
     }
 }
